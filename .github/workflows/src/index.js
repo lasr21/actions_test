b2c04a50a2fb597ac899d9671891acf3f2d87f62
lasr21 <lasr21@gmail.com>
2022-06-15 12:31:31 -0500

    test v12 1st PR

--- .github/workflows/src/index.js
+++ .github/workflows/src/index.js
@@ -85,21 +85,25 @@ try {
 
 // Validation 1st PR
 
-const pass = process.env.GH_SECRET
-
 "use strict";
 var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : new P(function (resolve) { resolve(result.value); }).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
+  return new (P || (P = Promise))(function (resolve, reject) {
+      function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+      function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+      function step(result) { result.done ? resolve(result.value) : new P(function (resolve) { resolve(result.value); }).then(fulfilled, rejected); }
+      step((generator = generator.apply(thisArg, _arguments || [])).next());
+  });
 };
-
+var __importStar = (this && this.__importStar) || function (mod) {
+  if (mod && mod.__esModule) return mod;
+  var result = {};
+  if (mod != null) for (var k in mod) if (Object.hasOwnProperty.call(mod, k)) result[k] = mod[k];
+  result["default"] = mod;
+  return result;
+};
 Object.defineProperty(exports, "__esModule", { value: true });
-const core = require("@actions/core");
-const github = require("@actions/github");
+const core = __importStar(require("@actions/core"));
+const github = __importStar(require("@actions/github"));
 function run() {
     return __awaiter(this, void 0, void 0, function* () {
         try {
@@ -109,7 +113,7 @@ function run() {
                 throw new Error('Action must have at least one of issue-message or pr-message set');
             }
             // Get client and context
-            const client = new github.GitHub(pass);
+            const client = new github.GitHub(core.getInput('repo-token', { required: true }));
             const context = github.context;
             if (context.payload.action !== 'opened') {
                 console.log('No issue or PR was opened, skipping');
