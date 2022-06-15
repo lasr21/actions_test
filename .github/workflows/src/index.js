e5ffaa9d81020a3906b3bbc69947590d2209a504
lasr21 <lasr21@gmail.com>
2022-06-15 11:32:08 -0500

    test v2 1st PR

--- .github/workflows/src/index.js
+++ .github/workflows/src/index.js
@@ -85,15 +85,30 @@ try {
 
 // Validation 1st PR
 
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : new P(function (resolve) { resolve(result.value); }).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (Object.hasOwnProperty.call(mod, k)) result[k] = mod[k];
+    result["default"] = mod;
+    return result;
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 const core = __importStar(require("@actions/core"));
 const github = __importStar(require("@actions/github"));
-
 function run() {
     return __awaiter(this, void 0, void 0, function* () {
         try {
-            const issueMessage = "first issue";
-            const prMessage = "first PR"
+            const issueMessage = core.getInput('issue-message');
+            const prMessage = core.getInput('pr-message');
             if (!issueMessage && !prMessage) {
                 throw new Error('Action must have at least one of issue-message or pr-message set');
             }
