7d7882bf1fd80d1cf87c7dc39c0dbf1eb78721aa
lasr21 <lasr21@gmail.com>
2022-06-15 20:02:42 -0500

    fix path 2

--- .github/workflows/src/app/file-validator.js
+++ .github/workflows/src/app/file-validator.js
@@ -4,7 +4,7 @@ const yaml = require('yaml')
 const md = require('markdown-it')
 
 const pullAuthor = actionEvent.pull.user.login
-const expectedPath = `data/${pullAuthor}`
+const expectedPath = `data/`
 const characterLimits = {  quote: 140 }
 
 class FileVaidator {
